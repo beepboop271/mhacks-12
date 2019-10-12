aa0771582b8b9e4e778ca3520d60b713e380ea07
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2019-10-12 17:08:46 -0400

    Better Path

--- Chrome-Extension/scripts/contentScript.js
+++ Chrome-Extension/scripts/contentScript.js
@@ -3,11 +3,10 @@ console.log("ASDASDASD");
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.msg === "note") {
-            let name = window.location.hostname;
-            name = name.substr(name.indexOf("www.") + 4);
-            name = name.substr(0, name.indexOf("."));
+            let name = window.location.href;
+            let projectName = "first-project";
              postNoteData({
-                path: "kill-me." + name,
+                path: projectName + "***" + name,
                 phrase: request.data.selection,
                 index: 0,
                 comment: "no comment"
